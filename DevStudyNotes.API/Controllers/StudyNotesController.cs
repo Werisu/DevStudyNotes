65aeb85522262d63707f1d9f7512fc1f0fff4837
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-12-01 08:51:47 -0300

    Adicionar arquivos de projeto.

---
+++ DevStudyNotes.API/Controllers/StudyNotesController.cs
@@ -0,0 +1,75 @@
+﻿using DevStudyNotes.API.Entities;
+using DevStudyNotes.API.Models;
+using DevStudyNotes.API.Persistnece;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStudyNotes.API.Controllers
+{
+    [ApiController]
+    [Route("api/study-notes")]
+    public class StudyNotesController : ControllerBase
+    {
+        private readonly StudyNoteDbContext _context;
+
+        public StudyNotesController(StudyNoteDbContext context)
+        {
+            this._context = context;
+        }
+        // api/study-notes
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var studyNotes = _context.StudyNotes.ToList();
+
+            return Ok(studyNotes);
+        }
+
+        // api/study-notes/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            // talvez retorna NotFound
+            var studyNote = _context.StudyNotes
+                .Include(s => s.Reactions)
+                .SingleOrDefault(s => s.Id == id);
+
+            if (studyNote == null)
+            {
+                return NotFound();
+            }
+            return Ok(studyNote);
+        }
+
+        // api/study-notes
+        [HttpPost]
+        public IActionResult Post(AddStudyNoteInputModel model)
+        {
+            // talvez retorne bad request
+            var studyNote = new StudyNote(model.Title, model.Description, model.isPublic);
+
+            _context.StudyNotes.Add(studyNote);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetById", new { id = studyNote.Id }, model);
+        }
+
+        // api/study-notes/1/reactions
+        [HttpPost("{id}/reactions")]
+        public IActionResult PostReaction(int id, AddReactionStudyNoteInputModel model) 
+        {
+            // Talvez not found / bad request
+            var studyNotes = _context.StudyNotes.SingleOrDefault(s => s.Id == id);
+
+            if(studyNotes == null)
+            {
+                return BadRequest();
+            }
+
+            studyNotes.AddReaction(model.IsPositive);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevStudyNotes.API/Persistnece/StudyNoteDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DevStudyNotes.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStudyNotes.API.Persistnece
+{
+    public class StudyNoteDbContext : DbContext
+    {
+        private readonly DbContextOptions<StudyNoteDbContext> _options;
+
+        public StudyNoteDbContext(DbContextOptions<StudyNoteDbContext> options) : base(options)
+        {
+            this._options = options;
+        }
+
+        public DbSet<StudyNote> StudyNotes { get; set; }
+        public DbSet<StudyNoteReaction> StudyNoteReactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StudyNote>(e =>
+            {
+                e.HasKey(s => s.Id);
+
+                e.HasMany(s => s.Reactions)
+                .WithOne()
+                .HasForeignKey(s => s.StudyNoteId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            builder.Entity<StudyNoteReaction>(sn =>
+            {
+                sn.HasKey(s => s.Id);
+            });
+        }
+    }
+}
